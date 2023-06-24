ed23585cce1bd8929d69ac8e39c3cf3538416ba6
evanmm3 <evanmm3@illinois.edu>
2023-06-24 14:39:16 -0500

    fixes to wave_plot axis markers

--- audioplot.py
+++ audioplot.py
@@ -6,41 +6,37 @@ from matplotlib import cm
 
 ## FUNCTIONS
 
-def plot_range(dist: list[int or float] = (0, 1), bins: int = 0, decimal: int = 0) -> list[int or float]:
+def plot_range(dist: list[int or float] = (0, 1), bins: int = 0, divisor: int = 1, decimal: int = 2):
     """
-    Given a range to mark on an axis, returns a list of evenly-spaced values for the axis.
+    Given a range to mark on an axis, returns a list of evenly-spaced values for the axis, with approximated labels.
+    """
 
-    Args:
-        dist (list, optional): range of values to span in axis. Defaults to [0, 1].
-        bins (int, optional): number of markers to divide the axis evenly by. If bins=0, an empty range is given. Defaults to 3.
-        decimal (int, optional): number of decimal places to round values to. Defaults to 2.
-
-    Returns:
-        list[int or float]: list of value markers. if round=0, value type is int.
-    """  
-    if bins <= 0: 
-        return []
-    elif bins == 1: 
-        return [dist[0]]
+    if bins <= 0: # If empty, provide no markers or labels
+        return [], []
+    elif bins == 1:
+        result = [dist[0], dist[1]]
     else:
-        return [round(i, decimal) for i in range(dist[0], dist[1], dist[1] // (bins - 1))] + [dist[1]]
+        result = [i for i in range(dist[0], dist[1], dist[1] // (bins - 1))] + [dist[1]]
+    labels = [f'${round(i / divisor, decimal)}$' for i in result]
+    return result, labels
 
 def wavread(path):
     sr, s = read(path)
     return sr, s.astype('float32')/32768
 
+
 # Plot waveform of an audio file
-def wave_plot(plot, sample, xbins=2, ybins=2,
-              xbinlabels=[], ybinlabels=[],
-              xlabel='time (s)', ylabel='Amplitude', title='Waveform',
+def wave_plot(plot, sample, sr=48000, xbins=2, ybins=2,
+              xbinlabels=[], ybinlabels=['$-\\alpha$', '$0.0$', '$\\alpha$'],
+              xlabel='samples', ylabel='Amplitude', title='Waveform',
               grid=False, color='C0'):
     plot.plot(sample, color=color)
 
-    xrange = plot_range([0, len(sample)], xbins)
-    plot.set_xticks(xrange, labels=xbinlabels if xbinlabels else [f'${i}$' for i in range(len(xrange))])
-
-    yrange = plot_range((round(np.min(sample)), round(np.max(sample))), ybins)
-    plot.set_yticks(yrange, labels=ybinlabels if ybinlabels else [f'${i}$' for i in range(len(yrange))])
+    # Axis markers and labels
+    xrange, xlabels = plot_range([0, len(sample)], bins=xbins, divisor=sr, decimal=2)
+    plot.set_xticks(xrange, labels=xbinlabels if (len(xbinlabels) == len(xrange)) else xlabels)
+    yrange, ylabels = plot_range([round(np.min(sample)), round(np.max(sample))], bins=2, decimal=2)
+    plot.set_yticks(yrange, labels=ybinlabels if (len(ybinlabels) == len(yrange)) else ylabels)
 
     # Plot appearance
     plot.tick_params(axis='both', which='major', labelsize=15)
@@ -50,21 +46,34 @@ def wave_plot(plot, sample, xbins=2, ybins=2,
     plot.set_ylabel(ylabel, fontsize=20)
     plot.set_title(title)
 
+
 # Plot waveform of an audio file
-def spec_plot(plot, sample, sr, interval, nfft=2048):
+def spec_plot(plot, sample, sr, nfft=2048, xbins=2, ybins=2,
+              xbinlabels=[], ybinlabels=[],
+              xlabel='time (s)', ylabel='Frequency (Hz)', title='Spectrogram', cmap=cm.viridis):
+    
     f, t, s = spectrogram(sample, sr, axis=0, nfft=nfft)
-    plot.pcolormesh(t, f, 10.0*np.log10(s), shading="auto")
-    plot.locator_params(axis='x', nbins=interval*2)
+
+    print(f.shape)
+    print(t.shape)
+    print(sample.shape[0]/sr)
+
+    plot.pcolormesh(t, f, 10.0*np.log10(s), shading="auto", cmap=cmap)
+    plot.locator_params(axis='x', nbins=xbins*2)
     # plot.set_xlabel('time (s)')
-    freq_range = range(0, sr//2, 1000*interval)
-    plot.set_xticks([])
-    plot.set_yticks([])
-    # plot.set_yticks([i for i in freq_range], [f'{i//1000}' for i in freq_range])
-    # plot.set_ylabel('Frequency (kHz)')
-    # plot.set_title('Spectrogram')
+    freq_range = range(0, sr//2, 1000*ybins)
+   
+   # Plot appearance
+    plot.tick_params(axis='both', which='major', labelsize=15)
+    plot.set_frame_on(False)
+    plot.set_xlabel(xlabel, fontsize=20)
+    plot.set_ylabel(ylabel, fontsize=20)
+    plot.set_title(title)
+
 
 def spec3d_plot(plot, sample, sr, interval, nfft=2048):
     f, t, s = spectrogram(sample, sr, axis=0, nfft=nfft)
+    # I don't know why this line works, it just does
     plot.plot_surface(f[:, None], t[None, :], 10.0*np.log(s), cmap=cm.viridis)
     # plot.set_title('3D Spectrogram')
     freq_range = range(0, sr//2, 1000*interval)
