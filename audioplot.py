709caac3fb190c9040e82a06a6a27bbb15a82f75
evanmm3 <evanmm3@illinois.edu>
2023-06-25 15:04:00 -0500

    changes to spec, spec3d plotting

--- audioplot.py
+++ audioplot.py
@@ -20,6 +20,7 @@ def plot_range(dist: list[int or float] = (0, 1), bins: int = 0, divisor: int = 
     labels = [f'${round(i / divisor, decimal)}$' for i in result]
     return result, labels
 
+# Read in .wav files as sample rate (integer), and array of values.
 def wavread(path):
     sr, s = read(path)
     return sr, s.astype('float32')/32768
@@ -47,42 +48,62 @@ def wave_plot(plot, sample, sr=48000, xbins=2, ybins=2,
     plot.set_title(title)
 
 
+# STFT Function
+DFT_SIZE = 2048
+HOP_SIZE = 256
+WINDOW = np.hanning(DFT_SIZE)
+from scipy import fft
+def stft( input_sound, dft_size=DFT_SIZE, hop_size=HOP_SIZE, zero_pad=0, window=WINDOW):
+    frames = []
+    # zero-padding for equally sized frames
+    input_sound = np.append(input_sound, np.zeros(len(input_sound) % dft_size))
+
+    for i in range(0, len(input_sound) - dft_size, hop_size):
+        frame = input_sound[i : i + dft_size] * window
+        frames.append(frame)
+
+    spectrogram = np.array([(fft.rfft(f, dft_size + zero_pad)) for f in frames], dtype=np.complex64)
+    # Return a complex-valued spectrogram (frequencies x time)
+    return spectrogram.T 
+
+
 # Plot waveform of an audio file
 def spec_plot(plot, sample, sr, nfft=2048, xbins=2, ybins=2,
               xbinlabels=[], ybinlabels=[],
-              xlabel='time (s)', ylabel='Frequency (Hz)', title='Spectrogram', cmap=cm.viridis):
+              xlabel='time (s)', ylabel='Frequency (kHz)', title='Spectrogram', cmap=cm.viridis):
     
-    f, t, s = spectrogram(sample, sr, axis=0, nfft=nfft)
+    sample_stft = stft(sample, dft_size=nfft, hop_size=nfft//16)
 
-    print(f.shape)
-    print(t.shape)
-    print(sample.shape[0]/sr)
+    plot.pcolormesh(10.0*np.log(np.absolute(sample_stft)), shading="auto", cmap=cmap)
 
-    plot.pcolormesh(t, f, 10.0*np.log10(s), shading="auto", cmap=cmap)
-    plot.locator_params(axis='x', nbins=xbins*2)
-    # plot.set_xlabel('time (s)')
-    freq_range = range(0, sr//2, 1000*ybins)
+    plot.set_xticks(np.linspace(0, sample_stft.shape[1], xbins))
+    plot.set_xticklabels(xbinlabels if xbinlabels else [np.round(i * len(sample) / (xbins - 1) / sr, 2) for i in range(xbins)])
+    plot.set_yticks(np.linspace(0, sample_stft.shape[0], ybins))
+    plot.set_yticklabels(ybinlabels if ybinlabels else np.linspace(0, sr/2000, ybins))
    
    # Plot appearance
     plot.tick_params(axis='both', which='major', labelsize=15)
-    plot.set_frame_on(False)
+    if xbins == 0:
+        plot.set_frame_on(False)
     plot.set_xlabel(xlabel, fontsize=20)
     plot.set_ylabel(ylabel, fontsize=20)
     plot.set_title(title)
 
 
-def spec3d_plot(plot, sample, sr, interval, nfft=2048):
+def spec3d_plot(plot, sample, sr, nfft=2048, xbins=2, ybins=2, zbins=2):
     f, t, s = spectrogram(sample, sr, axis=0, nfft=nfft)
+    s = 10.0*np.log(s)
+
     # I don't know why this line works, it just does
-    plot.plot_surface(f[:, None], t[None, :], 10.0*np.log(s), cmap=cm.viridis)
-    # plot.set_title('3D Spectrogram')
-    freq_range = range(0, sr//2, 1000*interval)
-    plot.set_xticks([])
-    plot.set_yticks([])
-    plot.set_zticks([])
+    plot.plot_surface(t[None, :], f[:, None], s, cmap=cm.viridis)
+
+    plot.set_xticks(np.linspace(0, len(sample)//sr, xbins))
+    plot.set_yticks(np.linspace(0, sr/2, ybins))
+    plot.set_zticks(np.linspace(np.min(s), 0, zbins))
     # plot.set_xticks([i for i in freq_range], [f'{i//1000}' for i in freq_range])
-    # plot.set_xlabel('Frequency (kHz)')
-    # plot.set_ylabel('time(s)')
+    plot.set_ylabel('Frequency (kHz)')
+    plot.set_xlabel('time(s)')
+    plot.set_zlabel('Intensity (dB)')
 
 
 # def audioplot(sample, sr):
